64d59ed53b90ab3e1f113bd20dbcd09355448a92
janadari <janadari1995@gmail.com>
2025-03-01 21:23:04 +0530

    home screen with data list

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,12 +1,99 @@
 import React, { useState } from "react";
-import { Text } from "react-native-gesture-handler";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import FastImage from "react-native-fast-image";
 
-const NewsCard = (item) => {
+const NewsCard = ({ item }) => {
+  const formatDate = (timestamp) => {
+    return new Date(timestamp).toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
+  const openLink = () => {
+    console.log("te", item);
+
+    Linking.openURL(item?.url).catch((err) =>
+      console.error("Failed to open URL: ", err)
+    );
+  };
+
   return (
-    <>
-      <Text>hi</Text>
-    </>
+    <TouchableOpacity style={styles.container} onPress={() => openLink()}>
+      <View style={styles.flexContainer}>
+        <View style={styles.flexLeft}>
+          <FastImage
+            style={styles.img}
+            source={{
+              uri: item?.image,
+              priority: FastImage.priority.normal,
+            }}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+        </View>
+        <View style={styles.flexRight}>
+          <View style={styles.innerContainer}>
+            <Text style={styles.intro}>{item.source.toUpperCase()}</Text>
+            <Text style={styles.intro}>{formatDate(item.datetime)}</Text>
+          </View>
+          <Text style={styles.description}>
+            {item.headline.length > 90
+              ? item.headline.substring(0, 90) + "..."
+              : item.headline}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { paddingBottom: 16 },
+  flexContainer: {
+    paddingTop: 16,
+    paddingBottom: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  flexLeft: {
+    flex: 1,
+  },
+  flexRight: {
+    flex: 2,
+  },
+  intro: {
+    color: "#ffffff",
+    fontFamily: "Rubic",
+    fontWeight: 400,
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  description: {
+    color: "#ffffff",
+    fontFamily: "Roboto",
+    fontWeight: 500,
+    fontStyle: 20,
+    lineHeight: 24,
+  },
+  img: {
+    width: 100,
+    height: 100,
+  },
+  innerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 4,
+  },
+});
 
 export default NewsCard;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import useUserName from "../hooks/useUserName";
 import { FINNHUB_API_KEY } from "@env";
 import NewsCard from "../components/NewsCard";
+import { ScrollView } from "react-native-gesture-handler";
 
 const HomeScreen = () => {
-  const userName = useUserName();
+  const { userName } = useUserName();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
+    console.log("username-home", userName);
+  });
+  useEffect(() => {
+    console.log("username", userName);
+
     const fetchData = async () => {
       let datalist = await getData();
-      setData(datalist || []); // Ensure data is always an array
+      setData(datalist || []);
       setLoading(false);
     };
 
@@ -32,29 +38,41 @@ const HomeScreen = () => {
       }
 
       const data = await response.json();
-      console.log("data", data);
-
+      console.log(data);
       return data;
     } catch (error) {
       console.error("Fetch error:", error);
+      setErrorMsg("Something went wrong. Please try again later.");
       return null;
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <SafeAreaView style={styles.sectionContainer}>
       <View style={styles.container}>
-        <Text>Hey {userName}</Text>
+        <Text style={styles.home_title}>Hey {userName}</Text>
 
-        {loading ? (
-          <Text>loading..</Text>
-        ) : (
-          <View>
-            {data.map((item, index) => (
-              <NewsCard key={index} item={item} />
-            ))}
-          </View>
-        )}
+        <ScrollView style={{ flex: 1, marginTop: 16 }}>
+          {loading ? (
+            <View style={{ flex: 1, justifyContent: "flex-start" }}>
+              <ActivityIndicator size={"large"} />
+            </View>
+          ) : (
+            <View>
+              {errorMsg ? (
+                <Text style={styles.errorMsg}>{errorMsg}</Text>
+              ) : (
+                <View>
+                  {data.map((item, index) => (
+                    <NewsCard key={index} item={item} />
+                  ))}
+                </View>
+              )}
+            </View>
+          )}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -67,8 +85,23 @@ const styles = StyleSheet.create({
   },
   sectionContainer: {
     flex: 1,
-    padding: 24,
     height: "100%",
+    backgroundColor: "#05021B",
+  },
+  home_title: {
+    color: "#FFFFFF",
+    fontWeight: "900",
+    fontFamily: "Roboto",
+    fontSize: 32,
+    lineHeight: 35.2,
+    marginBottom: 16,
+  },
+  errorMsg: {
+    color: "#FFFFFF",
+    fontFamily: "Rubik",
+    fontWeight: 500,
+    fontSize: 16,
+    lineHeight: 24,
   },
 });
 export default HomeScreen;
